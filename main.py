9d2f92c406ef7c383448ca7f7bd93833bf9d7c0e
Jai Viral Doshi <62877713+Jai-Doshi@users.noreply.github.com>
2023-07-26 16:20:55 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from time import sleep
+pygame.init()
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.mixer.init()
+pygame.mixer.music.load('rom.mpeg')
+pygame.mixer.music.play()
+sleep(10)
+pygame.mixer.music.load('scary.mp3')
+pygame.mixer.music.play()
+sleep(1)
+image = pygame.image.load('scr.jpg')
+window.blit(image, (0,0))
+pygame.display.update()
+sleep(3)
